76b849d21325b143d12a0706746da35d56eb886a
HaapyDeng <jzhou_deng@163.com>
2017-05-09 17:55:55 +0800

    websocket集成

--- app/src/main/java/com/max_plus/smart/MainActivity.java
+++ app/src/main/java/com/max_plus/smart/MainActivity.java
@@ -1,14 +1,25 @@
 package com.max_plus.smart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.koushikdutta.async.ByteBufferList;
+import com.koushikdutta.async.DataEmitter;
+import com.koushikdutta.async.callback.CompletedCallback;
+import com.koushikdutta.async.callback.DataCallback;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.WebSocket;
 
 import java.util.ArrayList;
 
@@ -16,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private GridLayoutManager mGridLayoutManager;
     private ArrayList<UserBean> data = new ArrayList<UserBean>();
+    public static String wsUrl = "ws://192.168.1.103:9502";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +35,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initData();
         initView();
+
     }
 
     private void initData() {
-        for (int i = 0; i < 49; i++) {
-            UserBean user = new UserBean();
-            user.setName("小明明");
-            user.setState("缺勤");
-            user.setImg(R.drawable.img_absenteeism);
-            data.add(user);
-        }
+
+        AsyncHttpClient.getDefaultInstance().websocket(wsUrl, "my-protocol", new AsyncHttpClient.WebSocketConnectCallback() {
+            @Override
+            public void onCompleted(Exception ex, WebSocket webSocket) {
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                Log.d("mac==>>", getLocalMacAddress());
+                webSocket.send(getLocalMacAddress());
+                webSocket.send(new byte[10]);
+                webSocket.setStringCallback(new WebSocket.StringCallback() {
+                    public void onStringAvailable(String s) {
+                        System.out.println("I got a string: " + s);
+
+
+                    }
+                });
+                webSocket.setClosedCallback(new CompletedCallback() {
+                    @Override
+                    public void onCompleted(Exception ex) {
+                        Log.d("websocket连接失败", ex.toString());
+                        initData();
+                    }
+                });
+                webSocket.setDataCallback(new DataCallback() {
+                    public void onDataAvailable(DataEmitter emitter, ByteBufferList byteBufferList) {
+                        System.out.println("I got some bytes!");
+                        byteBufferList.recycle();
+                    }
+                });
+            }
+        });
+
     }
 
     private void initView() {
@@ -83,5 +123,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    public String getLocalMacAddress() {
+        WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = wifi.getConnectionInfo();
+        return info.getMacAddress();
     }
 }

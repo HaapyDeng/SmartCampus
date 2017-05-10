d508a93883ac0aef4f5dc69b859a919594c3d9ad
HaapyDeng <jzhou_deng@163.com>
2017-05-10 17:28:23 +0800

    websocket发送数据到服务器端及接受服务器端数据处理

--- app/src/main/java/com/max_plus/smart/MainActivity.java
+++ app/src/main/java/com/max_plus/smart/MainActivity.java
@@ -21,6 +21,10 @@ import com.koushikdutta.async.callback.DataCallback;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.WebSocket;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private GridLayoutManager mGridLayoutManager;
     private ArrayList<UserBean> data = new ArrayList<UserBean>();
     public static String wsUrl = "ws://192.168.1.103:9502";
+    private int state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,19 +47,46 @@ public class MainActivity extends AppCompatActivity {
 
         AsyncHttpClient.getDefaultInstance().websocket(wsUrl, "my-protocol", new AsyncHttpClient.WebSocketConnectCallback() {
             @Override
-            public void onCompleted(Exception ex, WebSocket webSocket) {
+            public void onCompleted(Exception ex, final WebSocket webSocket) {
                 if (ex != null) {
                     ex.printStackTrace();
                     return;
                 }
-                Log.d("mac==>>", getLocalMacAddress());
-                webSocket.send(getLocalMacAddress());
-                webSocket.send(new byte[10]);
+                Log.d("mac==>>", jsonMacData());
+                webSocket.send(jsonMacData());
                 webSocket.setStringCallback(new WebSocket.StringCallback() {
                     public void onStringAvailable(String s) {
                         System.out.println("I got a string: " + s);
-
-
+                        try {
+                            JSONObject jsonObject = new JSONObject(s);
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                UserBean user = new UserBean();
+                                JSONObject jsondata = jsonArray.getJSONObject(i);
+                                user.setName(jsondata.getString("username"));
+                                state = jsondata.getInt("state");
+                                switch (state) {
+                                    case 0:
+                                        user.setState(String.valueOf(R.string.absence));
+                                        user.setImg(R.drawable.img_absenteeism);
+                                        break;
+                                    case 1:
+                                        user.setState(String.valueOf(R.string.normal));
+                                        user.setImg(R.drawable.img_normal);
+                                        break;
+                                    case 2:
+                                        user.setState(String.valueOf(R.string.late));
+                                        user.setImg(R.drawable.img_late);
+                                        break;
+                                    default:
+                                        user.setState(String.valueOf(R.string.absence));
+                                        user.setImg(R.drawable.img_absenteeism);
+                                }
+                                data.add(user);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
                 webSocket.setClosedCallback(new CompletedCallback() {
@@ -129,5 +161,23 @@ public class MainActivity extends AppCompatActivity {
         WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         WifiInfo info = wifi.getConnectionInfo();
         return info.getMacAddress();
+    }
+
+    public String jsonMacData() {
+        String jsonresult = "";//定义返回字符串
+        JSONObject object = new JSONObject();//创建一个总的对象，这个对象对整个json串
+        try {
+            JSONArray jsonarray = new JSONArray();//json数组，里面包含的内容为pet的所有对象
+            JSONObject jsonObj = new JSONObject();//pet对象，json形式
+            jsonObj.put("mac", getLocalMacAddress());//向pet对象里面添加值
+            // 把每个数据当作一对象添加到数组里
+            jsonarray.put(jsonObj);//向json数组里面添加pet对象
+            object.put("data", jsonarray);//向总对象里面添加包含pet的数组
+            jsonresult = object.toString();//生成返回字符串
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("生成的json串为:", jsonresult);
+        return jsonresult;
     }
 }
